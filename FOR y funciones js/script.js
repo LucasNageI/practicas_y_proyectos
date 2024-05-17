e474e48d80e9d9ac9170dfe5c147fdca2b062cf7
Lucas Nagel <lucasanagel@gmail.com>
2024-05-16 21:56:32 -0300

    ejercicio suma, multiplicacion, resta y division

--- FOR y funciones js/script.js
+++ FOR y funciones js/script.js
@@ -1,21 +1,46 @@
-function calculadora (numero1, numero2, operacion) {
-    if (operacion === "multiplicar"){
-    let resultadoMultiplicacion = (numero1 * numero2)
-    return (resultadoMultiplicacion)
-    }
-    else if (operacion === "sumar"){
+function suma (numero1, numero2) {
     let resultadoSuma = (numero1 + numero2)
     return (resultadoSuma)
-    }
-    else if (operacion === "dividir"){
-    let resultadoDivision = (numero1 / numero2)
-    return (resultadoDivision)
-    }
-    else if (operacion === "restar"){
+}
+
+function multiplicacion (numero1, numero2) {
+    let resultadoMultiplicacion = (numero1 * numero2)
+    return (resultadoMultiplicacion)
+}
+
+function resta (numero1, numero2) {
     let resultadoResta = (numero1 - numero2)
     return (resultadoResta)
-    }
-    else {
-        let error = Number(prompt("error, dato no valido, vuelva a intentarlo"))
-    }
+}
+
+function division (numero1, numero2) {
+    let resultadoDivision = (numero1 / numero2)
+    return (resultadoDivision)
+}
+
+let operacion = prompt("Que operacion desea realizar?")
+
+while (operacion != "suma" && operacion != "multiplicacion" && operacion != "resta" && operacion != "division") {
+    operacion = prompt("operacion no reconocida, vuelva a intentarlo")
+}
+
+if(operacion === "suma") {
+    let numero1 = Number(prompt("Ingrese el primer numero a sumar"))
+    let numero2 = Number(prompt("Ingrese el segundo numero"))
+    console.log(suma(numero1, numero2))
+}
+else if(operacion === "multiplicacion"){
+    let numero1 = Number(prompt("Ingrese el primer numero a multiplicar"))
+    let numero2 = Number(prompt("Ingrese el segundo numero"))
+    console.log(multiplicacion(numero1, numero2))
+}
+else if(operacion === "resta"){
+    let numero1 = Number(prompt("Ingrese el primer numero a restar"))
+    let numero2 = Number(prompt("Ingrese el segundo numero"))
+    console.log(resta(numero1, numero2))
+}
+else if(operacion === "division"){
+    let numero1 = Number(prompt("Ingrese el primer numero a dividir"))
+    let numero2 = Number(prompt("Ingrese el segundo numero"))
+    console.log(division(numero1, numero2))
 }
