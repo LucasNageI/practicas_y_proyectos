f5a19340e7bae6d5144026dabdfde77d2b0a3b77
LucasNagel <lucasanagel@gmail.com>
2024-06-14 06:45:26 -0300

    funciones DEL y AC

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,5 +1,9 @@
 const formulario = document.getElementById("formulario")
 
+let string_num = ""
+
+let numero = ""
+
 const ACCIONES = {
     SUMAR: {
         accion: (a, b) => a + b
@@ -14,27 +18,46 @@ const ACCIONES = {
         accion: (a, b) => a / b
     },
     AC: {
-        accion: (texto) => texto = ""
+        accion: () => {
+            string_num.replaceAll(string_num, "") 
+            string_num = ""
+        }
     },
     DEL: {
-        accion: (texto) => texto.trimEnd() 
+        accion: () => {
+            let string_modificado = string_num.slice(0, -1)
+            string_num = string_modificado
+        } 
     }
 }
 
 const handleSubmit = (event) => {
     event.preventDefault()
-    const submit_btn = event.target
     console.log(event.target)
 }
 
-let string_num = ""
-
 const handleCalculardora = (event) => {
-    const numero = event.target.value
-    string_num = string_num + numero
-    resultado.innerText = string_num
-    if(event.target === DEL){
-        ACCIONES.DEL.accion(string_num)
+
+    if((typeof(Number(event.target.value) === 1, 2, 3, 4, 5, 6, 7, 8, 9, 0))){
+        numero = event.target.value
+        if(numero === undefined || numero === AC || numero === DEL){
+            string_num = string_num + ""
+            resultado.innerText = string_num
+        }
+        else{
+            if(event.target === formulario.DEL){
+                ACCIONES.DEL.accion()
+                resultado.innerText = string_num
+            }
+            else if(event.target === formulario.AC){
+                ACCIONES.AC.accion()
+                resultado.innerText = string_num
+            }
+            else{
+            string_num = string_num + numero
+            resultado.innerText = string_num
+            }
+        }
     }
 
 }
